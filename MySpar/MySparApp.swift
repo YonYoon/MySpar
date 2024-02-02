4dfd1b7e7063eb94347892fb6774b5ca52b227c6
YonYoon <zhzhzh1806@gmail.com>
2024-02-02 17:38:36 +0500

    Initial Commit

---
+++ MySpar/MySparApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySparApp.swift
+//  MySpar
+//
+//  Created by Zhansen Zhalel on 02.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MySparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
