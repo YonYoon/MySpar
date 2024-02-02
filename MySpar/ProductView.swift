e7a4923f5ee3bbafd1ceb6fbb29f2ac97fad5c50
YonYoon <zhzhzh1806@gmail.com>
2024-02-02 17:42:38 +0500

    Added ProductView

---
+++ MySpar/ProductView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductView.swift
+//  MySpar
+//
+//  Created by Zhansen Zhalel on 02.02.2024.
+//
+
+import SwiftUI
+
+struct ProductView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductView()
+}
