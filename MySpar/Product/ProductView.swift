a003ed2cc494f012cf981a02911bc88ea89096e4
YonYoon <zhzhzh1806@gmail.com>
2024-02-02 21:46:10 +0500

    Added price and button to ProductView

--- MySpar/Product/ProductView.swift
+++ MySpar/Product/ProductView.swift
@@ -29,7 +29,47 @@ struct ProductView: View {
                         Text(type)
                     }
                 }
+//                .padding(.bottom, 5)
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("55.9")
+                            .font(.title)
+                            .fontWeight(.bold)
+                        Text("199,0")
+                            .strikethrough()
+                            .foregroundStyle(.secondary)
+                    }
+                    
+                    Spacer()
+                    
+                    RoundedRectangle(cornerRadius: 40)
+                        .containerRelativeFrame(.horizontal) { width, axis in
+                            width * 0.4
+                        }
+                        .frame(height: 50)
+                        .foregroundStyle(.accent)
+                        .overlay {
+                            HStack(spacing: 15) {
+                                Text("-")
+                                    .font(.largeTitle)
+                                
+                                VStack {
+                                    Text("1 шт")
+                                        .font(.title3)
+                                        .fontWeight(.bold)
+                                    Text("120,0₽")
+                                }
+                                
+                                Text("+")
+                                    .font(.largeTitle)
+                            }
+                            .foregroundStyle(.white)
+                        }
+                }
+                .padding(.bottom)
             }
+            .padding(.horizontal)
             .pickerStyle(.segmented)
         }
         .toolbar {
